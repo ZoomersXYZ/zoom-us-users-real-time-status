f3962e6dabdf21e736cd411ecd7e7eb865fac4c8
Saadh S <atextbooksituation@gmail.com>
2020-07-18 13:06:12 -0400

    whoops

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,10 +7,10 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, 
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL, 
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID, 
-  storageBucket: REACT_APP_STORAGE_BUCKET, 
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID, 
-  appId: REACT_APP_APP_ID, 
-  measurementId: REACT_APP_MEASREMENT_ID  
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, 
+  appId: process.env.REACT_APP_APP_ID, 
+  measurementId: process.env.REACT_APP_MEASREMENT_ID  
 };
 firebase.initializeApp( firebaseConfig );
 
