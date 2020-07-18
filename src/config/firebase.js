09616d2ece095a40b50a65113a15bec46063c9ab
Saadh S <atextbooksituation@gmail.com>
2020-07-18 12:15:13 -0400

    - Removed dotenv. CRA enables env variables - Took hours to finally figure out two annoying issues that turned out to be bad Firebase config and bad internet connection.   - Fixed up Firebase, enabled analytics + offline (though React app isn't offline yet)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,27 +1,33 @@
 import firebase from 'firebase/app';
+import 'firebase/analytics';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, 
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL, 
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID, 
+  storageBucket: REACT_APP_STORAGE_BUCKET, 
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID, 
+  appId: REACT_APP_APP_ID, 
+  measurementId: REACT_APP_MEASREMENT_ID  
 };
-
 firebase.initializeApp( firebaseConfig );
+
 const db = firebase.firestore();
 
-// firebase.firestore().enablePersistence()
-//   .catch( function( err ) {
-//       if ( err.code === 'failed-precondition' ) {
-//           // Multiple tabs open, persistence can only be enabled
-//           // in one tab at a a time.
-//           // ...
-//       } else if ( err.code === 'unimplemented' ) {
-//           // The current browser does not support all of the
-//           // features required to enable persistence
-//           // ...
-//       }
-//   } );
+firebase.firestore().enablePersistence()
+  .catch( function( err ) {
+      if ( err.code === 'failed-precondition' ) {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if ( err.code === 'unimplemented' ) {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  } );
 // Subsequent queries will use persistence, if it was enabled successfully
 
 export { firebase, db as default };
