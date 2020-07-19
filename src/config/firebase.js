4e34c6a7bf567f52189bdff52ec89c282534b5ca
Saadh S <atextbooksituation@gmail.com>
2020-07-19 15:51:58 -0400

    - Removed offline - A few tiny things:   yarn commands, git ignore .firebase, fix public folder in firebase config
    - Mino

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -16,13 +16,4 @@ firebase.initializeApp( firebaseConfig );
 
 const db = firebase.firestore();
 
-firebase.firestore().enablePersistence()
-  .catch( function( err ) {
-      if ( err.code === 'failed-precondition' ) {
-          // Multiple tabs open, persistence can only be enabled in one tab at a a time.
-      } else if ( err.code === 'unimplemented' ) {
-          // The current browser does not support all of the features required to enable persistence
-      };
-  } );
-
 export { firebase, db as default };
