5dc578216eb7d5b507caa18d4bcefb887bc8ea5c
Saadh S <atextbooksituation@gmail.com>
2020-09-06 08:27:02 -0400

    Preliminary code of grabbing data based on new flat documents data structure

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import db from '../config/firebase';
 import UsersList from './UsersList';
 
+// import { useDocuments } from 'react-firebase-hooks/firestore';
+
 // const dreamy = async () => {
 //   let data = [];
 //   db.collection( 'groups' ).doc( nspName ).collection( 'log' ).orderBy( 'timestamp', 'desc' ).limit( 30 ).get().then( snap => {
@@ -20,31 +22,42 @@ const List = () => {
   
   useEffect( () => {
     // Lowest numbers on top
-    const arrByBasicSort = ( arr, field ) => 
-      arr.sort( ( a, b ) => a[ field ] - b[ field ] );
+    // const arrByBasicSort = ( arr, field ) => 
+    //   arr.sort( ( a, b ) => a[ field ] - b[ field ] );
 
     // Core
-    const stream = db.collection( 'online' )
-      .doc( 'now' )
+    const d = new Date();
+    const oneDayAgo = d.setDate( d.getDate() - 1000 * 60 * 60 * 24 );
+    const stream = db.collection( 'log' ) 
+      .where( 'timestamp', '>', oneDayAgo ) 
+      .orderBy( 'timestamp', 'desc' ) 
       .onSnapshot( 
         doc => {
           setLoading( false );
-          if ( doc.data() && doc.data().list && Array.isArray( doc.data().list ) ) {
-            const tmpArr = doc.data().list;
-            if ( Array.isArray( tmpArr ) ) {
-              setList( 
-                arrByBasicSort( 
-                  tmpArr, 
-                  'timestamp' 
-                ) 
-              );
-            };
-          };
+          // console.log( 'hi first count', doc.forEach( solo => arr.push( solo.data() ) ); );
+          // console.log( 'hi first length', doc.data().length );
+          
+          console.log( 'hi second length' );
+
+          const arr = [];
+          doc.forEach( solo => arr.push( solo.data() ) );
+          setList( arr );
+          // if ( doc.data() && doc.data().list && Array.isArray( doc.data().list ) ) {
+            // const tmpArr = doc.data().list;
+            // if ( Array.isArray( tmpArr ) ) {
+              // setList( 
+              //   arrByBasicSort( 
+              //     tmpArr, 
+              //     'timestamp' 
+              //   ) 
+              // );
+            // };
+          // };
         },
         err => {
           console.log( err );
           setErr( err );
-        }      
+        } 
       );
       return () => stream();
   }, [] );
