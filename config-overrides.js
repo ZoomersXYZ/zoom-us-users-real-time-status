65554e27018df9d5711db393de5146b3d4d215a1
Saadh S <atextbooksituation@gmail.com>
2020-09-08 18:10:01 -0400

    Fixed some of the issues with Google Cloud emulator. It runs now.
    - Need a package.json where the functions file is and at the very least that package.json needs to state where the functions file is in main.
    - Correct the firebase.json source. I didn’t know the default for it is src. Is that documented somewhere specifically saying that’s the default in the code vs general talks of convention?
    - Fixed up the incorrect things for react rewired and customize-cra so it works now with a different directory

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
+/* eslint-disable */
+const { override } = require( 'customize-cra' );
+// const paths = require('react-scripts/config/paths');
 const path = require( 'path' );
 
 module.exports = {
-    paths: function ( paths, env ) {        
-        paths.appIndexJs = path.resolve( __dirname, 'src/app/index.js' );
-        paths.appSrc = path.resolve( __dirname, 'src/app' );
-        return paths;
-    } 
-}
+  paths: function ( paths, env ) { 
+    paths.appIndexJs = path.resolve( __dirname, 'src/app/index.js' );
+    paths.appSrc = path.resolve( __dirname, 'src/app' );
+    return paths;
+  } 
+};
+
+// module.exports = override(
+//   paths: thePaths()
+// );
