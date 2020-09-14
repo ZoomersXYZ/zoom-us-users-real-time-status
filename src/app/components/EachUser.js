b163a82129c04060c1b438123d6d7d142e888b35
Saadh S <atextbooksituation@gmail.com>
2020-09-14 10:35:54 -0400

    Minor adjustments to have frontend work. Cleaning up too.

--- src/app/components/EachUser.js
+++ src/app/components/EachUser.js
@@ -3,7 +3,7 @@ import { formatDistanceToNow } from 'date-fns';
 
 const EachUser = ( { aUser } ) => (
   <li className={ aUser.online ? 'active' : 'inactive' }>
-    <span className="handle">{ aUser.user_name }</span>, { } 
+    <span className="handle">{ aUser.handle }</span>, { } 
     <span className="time-on">{ formatDistanceToNow( Number( aUser.timestamp ), { includeSeconds: true } ) }</span> 
     { } ago
   </li>
--- src/app/components/List.js
+++ src/app/components/List.js
@@ -21,38 +21,19 @@ const List = () => {
   const [ list, setList ] = useState( null );
   
   useEffect( () => {
-    // Lowest numbers on top
-    // const arrByBasicSort = ( arr, field ) => 
-    //   arr.sort( ( a, b ) => a[ field ] - b[ field ] );
-
     // Core
     const d = new Date();
     const oneDayAgo = d.setDate( d.getDate() - 1000 * 60 * 60 * 24 );
     const stream = db.collection( 'log' ) 
       .where( 'timestamp', '>', oneDayAgo ) 
+      .where( 'dupe', '==', false ) 
       .orderBy( 'timestamp', 'desc' ) 
       .onSnapshot( 
         doc => {
           setLoading( false );
-          // console.log( 'hi first count', doc.forEach( solo => arr.push( solo.data() ) ); );
-          // console.log( 'hi first length', doc.data().length );
-          
-          console.log( 'hi second length' );
-
           const arr = [];
           doc.forEach( solo => arr.push( solo.data() ) );
           setList( arr );
-          // if ( doc.data() && doc.data().list && Array.isArray( doc.data().list ) ) {
-            // const tmpArr = doc.data().list;
-            // if ( Array.isArray( tmpArr ) ) {
-              // setList( 
-              //   arrByBasicSort( 
-              //     tmpArr, 
-              //     'timestamp' 
-              //   ) 
-              // );
-            // };
-          // };
         },
         err => {
           console.log( err );
