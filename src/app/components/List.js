2c53aa5253e174d37be13ab02d7b121257a42aa9
Saadh S <atextbooksituation@gmail.com>
2020-10-09 22:21:30 -0400

    Back to doing Firestore real time by hand. Using docChanges for first time.
    - docChanges allows not setting the entire array each time

--- src/app/components/List.js
+++ src/app/components/List.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import { sub } from 'date-fns';
+import React, { useEffect, useState } from 'react';
 
 import db from '../config/firebase';
 import EachUser from './EachUser';
+import { sub } from 'date-fns'
 
 const List = () => {
+  const [ error, setError ] = useState( false );
+  const [ loading, setLoading ] = useState( true );
+  const [ list, setList ] = useState( [] );
+  
+  useEffect( () => {
+    // Core
     const oneDayAgo = sub( new Date(), {
       days: 1 
     } ).getTime();
-    const [ value, loading, error ] = useDocumentData(
-      db.collectionGroup( 'rt_log' )
-        .where( 'timestamp', '>', oneDayAgo )
-        .where( 'dupe', '==', false )
-        .orderBy( 'timestamp', 'desc' )
-    );
+    const stream = db.collectionGroup( 'rt_log' )
+      .where( 'timestamp', '>', oneDayAgo ) 
+      .where( 'dupe', '==', false ) 
+      .orderBy( 'timestamp', 'desc' ) 
+      .onSnapshot( 
+        doc => {
+          doc.docChanges().forEach( ( change ) => {
+            if ( change.type === 'added' ) {
+              // console.log( 'New city: ', change.doc.data() );
+              setList( prev => {
+                prev.push( change.doc.data() );
+                return prev;
+                } );
+            }            
+          } );
+          setLoading( false );
+        },
+        err => {
+          console.log( error );
+          setError( error );
+        } 
+      );
+      return () => stream();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [] );
 
   return (
     <div>
@@ -22,16 +46,14 @@ const List = () => {
         && <span>Loading...</span> 
       }
       { error && 
-          <span>{ error }</span> 
+        <span>{ error }</span> 
       }
-
       <h3 className="online-count">
-        Online user count: { loading ? '0' : ( value && value.filter( han => han.online ).length ) } 
+        Online user count: { list && list.filter( han => han.online ).length } 
       </h3>
-
       <div>
         <ul className="user-list">
-          { !!value && value.map( ( aUser, index ) => 
+          { !!list && list.map( ( aUser, index ) => 
             <EachUser 
               key={ `auser-${ index }` } 
               aUser={ aUser } 
